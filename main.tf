a186d2831089d4c7c7629b307cd0a17d24e7b561
yogeshmadghe <yogeshmadghe@gmail.com>
2020-12-21 20:23:27 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,14 @@
-provider "aws" {
-  version = "2.33.0"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
 
+# Configure the AWS Provider
+provider "aws" {
   region = var.aws_region
 }
 
